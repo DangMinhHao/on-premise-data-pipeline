0fcf765bb3da7bf76c637fda69aee1cac81a01d5
haodang2311 <hao.dang@techxcorp.com>
2024-05-07 01:19:11 +0700

    Modify setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from setuptools import setup, config
 
 if __name__=="__main__":
-    setup(
-        name = 'OnPremiseDataPipeline',
-        version = 1.0,
-        author = 'hao-dang',
-        author_email = 'haodang2311@gmail.com',
-
-        install_requires = config.read('setup.cfg')['install_requires'],
-    )
+    setup()
